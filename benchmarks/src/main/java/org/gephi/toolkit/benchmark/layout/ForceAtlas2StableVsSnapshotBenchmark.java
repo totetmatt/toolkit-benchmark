31fde15d8dfaf972792f68ca81b55070af6c267f
totetmatt <matthieu.totet@gmail.com>
2026-08-25 17:00:11 +0200

    1 class with parametrization

--- benchmarks/src/main/java/org/gephi/toolkit/benchmark/layout/ForceAtlas2StableVsSnapshotBenchmark.java
+++ benchmarks/src/main/java/org/gephi/toolkit/benchmark/layout/ForceAtlas2StableVsSnapshotBenchmark.java
@@ -2,70 +2,58 @@ package org.gephi.toolkit.benchmark.layout;
 
 import java.util.concurrent.TimeUnit;
 import org.gephi.graph.api.GraphModel;
+import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder;
+import org.gephi.layout.spi.Layout;
 import org.gephi.toolkit.benchmark.RandomGraphs;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.*;
+import snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2;
 
 @BenchmarkMode(Mode.SampleTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(0)
 public class ForceAtlas2StableVsSnapshotBenchmark {
 
-    private static final int NODE_COUNT = 200;
 
-    @State(Scope.Thread)
-    public static class StableState {
+    @State(Scope.Benchmark)
+    public static class BenchmarkState  {
 
-        private org.gephi.layout.plugin.forceAtlas2.ForceAtlas2 layout;
+
+
+        @Param({"100", "1000", "5000"})
+        public int nodeCount;
+
+        @Param({"snapshot","stable"})
+        public String layout;
+
+        public Layout useLayout = null;
 
         @Setup(Level.Trial)
         public void setup() {
-            GraphModel graphModel = RandomGraphs.newRandomGraphModel(NODE_COUNT);
-            layout = new org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder().buildLayout();
-            layout.setGraphModel(graphModel);
-            layout.initAlgo();
+            GraphModel graphModel = RandomGraphs.newRandomGraphModel(nodeCount);
+
+            if(layout.equals("stable"))
+            {
+                useLayout= new ForceAtlas2Builder().buildLayout();
+            }
+            if(layout.equals("snapshot"))
+            {
+                useLayout = new snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder().buildLayout();
+            }
+            if(layout == null){
+                throw new RuntimeException("layout is null");
+            }
+            useLayout.setGraphModel(graphModel);
+            useLayout.initAlgo();
         }
 
         @TearDown(Level.Trial)
         public void tearDown() {
-            layout.endAlgo();
-        }
-    }
-
-    @State(Scope.Thread)
-    public static class SnapshotState {
-
-        private snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2 layout;
-
-        @Setup(Level.Trial)
-        public void setup() {
-            GraphModel graphModel = RandomGraphs.newRandomGraphModel(NODE_COUNT);
-            layout = new snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder().buildLayout();
-            layout.setGraphModel(graphModel);
-            layout.initAlgo();
-        }
-
-        @TearDown(Level.Trial)
-        public void tearDown() {
-            layout.endAlgo();
+            useLayout.endAlgo();
         }
     }
 
     @Benchmark
-    public void stable(StableState state) {
-        state.layout.goAlgo();
-    }
-
-    @Benchmark
-    public void snapshot(SnapshotState state) {
-        state.layout.goAlgo();
+    public void goAlgo(BenchmarkState state) {
+        state.useLayout.goAlgo();
     }
 }
