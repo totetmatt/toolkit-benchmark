d7032f8dc99697b9b6ff3cbe8279bac7f31cef6b
totetmatt <matthieu.totet@gmail.com>
2026-08-31 07:10:42 +0200

    Add more gexf

--- benchmarks/src/main/java/org/gephi/toolkit/benchmark/layout/ForceAtlas2StableVsSnapshotBenchmark.java
+++ benchmarks/src/main/java/org/gephi/toolkit/benchmark/layout/ForceAtlas2StableVsSnapshotBenchmark.java
@@ -8,9 +8,10 @@ import org.gephi.layout.spi.Layout;
 import org.gephi.toolkit.benchmark.GexfGraph;
 
 import org.openjdk.jmh.annotations.*;
+import snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2;
 
 
-@BenchmarkMode(Mode.SampleTime)
+@BenchmarkMode(Mode.SingleShotTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
 public class ForceAtlas2StableVsSnapshotBenchmark {
@@ -25,24 +26,34 @@ public class ForceAtlas2StableVsSnapshotBenchmark {
         @Param({"snapshot","stable"})
         public String layout;
 
+        @Param({"comic-hero-network.gexf","rfc.gexf","World_flight_routes.gexf"})
+        public String gexf;
+
+        @Param({"true","false"})
+        public boolean barnesHut;
         public Layout useLayout = null;
 
         @Setup(Level.Trial)
         public void setup() {
 
-            GraphModel graphModel = GexfGraph.loadGexf(GexfGraph.getFile("comic-hero-network.gexf"));
+            GraphModel graphModel = GexfGraph.loadGexf(GexfGraph.getFile(gexf));
 
             if(layout.equals("stable"))
             {
-                useLayout= new ForceAtlas2Builder().buildLayout();
+                org.gephi.layout.plugin.forceAtlas2.ForceAtlas2 current_layout = new ForceAtlas2Builder().buildLayout();
+                current_layout.setBarnesHutOptimize(barnesHut);
+                useLayout = current_layout;
             }
             if(layout.equals("snapshot"))
             {
-                useLayout = new snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder().buildLayout();
+                ForceAtlas2 current_layout = new snapshot.org.gephi.layout.plugin.forceAtlas2.ForceAtlas2Builder().buildLayout();
+                current_layout.setBarnesHutOptimize(barnesHut);
+                useLayout = current_layout;
             }
             if(layout == null){
                 throw new RuntimeException("layout is null");
             }
+
             useLayout.setGraphModel(graphModel);
             useLayout.initAlgo();
         }
